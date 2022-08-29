878b84f038000601e6aca533ac12dbdf747ce1a0
听日要早起 <xujinhuan_email@163.com>
2022-08-29 20:19:39 +0800

    feat:初步完成core结构

--- framework/core.go
+++ framework/core.go
@@ -1,16 +1,29 @@
 package framework
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 //Core 框架核心结构
 type Core struct {
-
+	router map[string]ControllerHandler
 }
 
 func NewCore() *Core {
-	return &Core{}
+	return &Core{router: map[string]ControllerHandler{}}
 }
 
 func (c *Core)ServeHTTP(response http.ResponseWriter,request *http.Request)  {
-	//TODD
+	log.Println("core.serveHTTP")
+	ctx := NewContext(request, response)
+
+	// 一个简单的路由选择器，这里直接写死为测试路由foo
+	router := c.router["foo"]
+	if router == nil {
+		return
+	}
+	log.Println("core.router")
+
+	router(ctx)
 }
